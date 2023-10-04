1ce38d524d66fbad3b707ef9d1fa968313f3d6d1
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-10-04 10:19:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// Write your code here'
+
+//Equation when multiplied is 20
+let num1=2,num2=31;
+let multiply= num1*num2;
+
+//random interger
+let random= Math.floor(Math.random()*10)+1;
+
+// calculate remainder to 4
+let num3=34, num4= 5;
+let mod = num3%num4;
+
+// return 20 as highest number
+let max = Math.max(10,8,15,20,17,6);
